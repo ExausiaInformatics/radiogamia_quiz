5be185170fa966b6142dea69ac31dca4dfc23199
RajeevKumarSinha <rajeev.exousia@gmail.com>
2021-02-17 18:35:17 +0530

    home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,20 +5,29 @@ import { Link } from 'react-router-dom';
 const Home = () => (
     <Fragment>
         <Helmet><title>Home - Quiz App</title></Helmet>
-        <div id="home">
+        <div id="home" className="bg-dark">
             <section>
-                <div style={{ textAlign: 'center' }}>
-                    <span className="mdi mdi-cube-outline cube"></span>
-                </div>
-                <h1>Quiz App</h1>
-                <div className="play-button-container">
-                    <ul>
-                        <li><Link className="play-button" to="/play/instructions">Play</Link></li>
-                    </ul>
+                <div style={{ textAlign: 'center' }} className="mt-5">
+                <h1 className="cube">Radiogamia</h1>
                 </div>
                 <div className="auth-container">
-                    <Link to="/login" className="auth-buttons" id="login-button">Login</Link>
-                    <Link to="/register" className="auth-buttons" id="signup-button">Sign up</Link>
+                    <Link to="/login" className="auth-buttons mx-auto mt-5" id="login-button">Login</Link>
+                </div> 
+                <div className="auth-container">   
+                    <Link to="/register" className="auth-buttons mx-auto" id="signup-button">Sign up</Link>
+                </div>
+                <div className="play-button-container">
+                    <ul>
+                        <li><Link className="play-button mx-auto" to="/play/instructions">Skip</Link></li>
+                    </ul>
+                </div>
+                <div className="row mar-contact">
+                    <div className="col-6">
+                        <a href="#" className="float-right aboutus">About Us</a>
+                    </div>
+                    <div className="col-6">
+                        <a href="#" className="aboutus">Contact</a>
+                    </div>
                 </div>
             </section>
         </div>
